bf8845a111979b7c32d946601bc1ef828bac9708
Paul Wood <pmwood@pm.me>
2022-04-23 17:51:13 -0400

    Added general layout

--- src/index.tsx
+++ src/index.tsx
@@ -19,21 +19,23 @@ function App() {
           title="Aether Sensor Network"
           links={[
             {
-              label: "link 1",
-              href: "https://example.com",
+              label: "Report",
+              href: "https://github.com/ucf-aether/report",
             },
             {
-              label: "link 2",
-              href: "https://example.com",
+              label: "Firmware",
+              href: "https://github.com/ucf-aether/aether-firmware",
             },
             {
-              label: "link 3",
-              href: "https://example.com",
+              label: "Electronics",
+              href: "https://github.com/ucf-aether/electronics",
+            },
+            {
+              label: "Application",
+              href: "https://github.com/ucf-aether/aether-app",
             },
           ]}
           enableDarkModeSwitch={true}
-          githubRepoUrl="https://github.com/torvalds/linux"
-          githubButtonSize="large"
         />
       }
       headerOptions={{
@@ -42,21 +44,23 @@ function App() {
       }}
       footer={
         <GlFooter
-          bottomDivContent="Licence M I T"
-          email="email@email.com"
-          phoneNumber="+33545345676"
+          bottomDivContent="Licence MIT"
           links={[
             {
-              href: "https://example.com",
-              label: "link 1",
+              label: "Report",
+              href: "https://github.com/ucf-aether/report",
             },
             {
-              href: "https://example.com",
-              label: "link 2",
+              label: "Firmware",
+              href: "https://github.com/ucf-aether/aether-firmware",
             },
             {
-              href: "https://example.com",
-              label: "link 3",
+              label: "Electronics",
+              href: "https://github.com/ucf-aether/electronics",
+            },
+            {
+              label: "Application",
+              href: "https://github.com/ucf-aether/aether-app",
             },
           ]}
         />
@@ -64,7 +68,7 @@ function App() {
     >
       <GlHero
         title="Aether Sensor Network"
-        subTitle="A device to measure the AQI"
+        subTitle="The Spring 2022 UCF CECS Senior Design Showcase winner, a LoRaWAN-enabled air quality sensor that measures and calculates the EPA Air Quality Index (AQI)."
         illustration={{
           type: "image",
           imageSrc:
@@ -75,17 +79,17 @@ function App() {
       />
 
       <GlArticle
-        id="firstSection"
-        title="Article title"
+        id="aboutAqi"
+        title="What is the AQI?"
         body={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
                     nisl nec hendrerit rutrum, 
                     mi enim **semper arcu**, ut imperdiet urna libero non metus. 
                     Donec imperdiet ac nulla sit amet lacinia. 
                     Suspendisse volutpat lectus vitae libero luctus, a egestas magna egestas. 
                     Suspendisse potenti. In semper erat scelerisque sapien convallis porttitor.`}
-        buttonLabel="Article Button label"
+        buttonLabel="Learn More"
         buttonLink={{
-          href: "https://example.com",
+          href: "https://www.airnow.gov",
         }}
         illustration={
           <GlIllustration
@@ -98,43 +102,56 @@ function App() {
         illustrationPosition="left"
       />
 
+      <GlSectionDivider />
+
+      <GlArticle
+        id="aboutAether"
+        title="What is Aether?"
+        body={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
+                    nisl nec hendrerit rutrum, 
+                    mi enim **semper arcu**, ut imperdiet urna libero non metus. 
+                    Donec imperdiet ac nulla sit amet lacinia. 
+                    Suspendisse volutpat lectus vitae libero luctus, a egestas magna egestas. 
+                    Suspendisse potenti. In semper erat scelerisque sapien convallis porttitor.`}
+        buttonLabel="Aether GitHub"
+        buttonLink={{
+          href: "https://www.github.com/ucf-aether",
+        }}
+        illustration={
+          <GlIllustration
+            hasShadow={false}
+            type="image"
+            url="https://user-images.githubusercontent.com/39378411/135731808-6cf3e4dd-1047-4a0a-95be-65fdd6947315.png"
+          />
+        }
+        hasAnimation={true}
+        illustrationPosition="right"
+      />
+
       <GlCards>
         {
           <>
             <GlLogoCard
-              title="Card title"
+              title="Designed in KiCad"
               paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
                                 nisl nec hendrerit rutrum, 
                                 mi enim semper arcu, ut imperdiet urna libero non metus.`}
               buttonLabel="Button Label"
-              iconUrls={[
-                "https://user-images.githubusercontent.com/39378411/135731999-a2d8f901-3d7d-40a9-b59f-102ee1facc45.png",
-                "https://user-images.githubusercontent.com/39378411/135731995-136d4baf-58a6-4cb3-a72c-b8ddce835b3c.png",
-              ]}
             />
             <GlLogoCard
-              title="Card title"
+              title="Powered by Zephyr RTOS"
               paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
                                 nisl nec hendrerit rutrum, 
                                 mi enim semper arcu, ut imperdiet urna libero non metus.`}
               buttonLabel="Button Label"
-              iconUrls={[
-                "https://user-images.githubusercontent.com/39378411/135731998-e01a7970-a7c4-4041-b07c-341e075207d2.png",
-              ]}
             />
 
             <GlLogoCard
-              title="Card title"
+              title="Accessible by AWS & Supabase"
               paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
                                 nisl nec hendrerit rutrum, 
                                 mi enim semper arcu, ut imperdiet urna libero non metus.`}
               buttonLabel="Button Label"
-              iconUrls={[
-                "https://user-images.githubusercontent.com/39378411/135731991-3da13e97-c2f7-42b2-88ab-055aff0d6ae9.png",
-                "https://user-images.githubusercontent.com/39378411/135731994-29a3c46a-0d92-4ec8-954e-39bfeeb06534.png",
-                "https://user-images.githubusercontent.com/39378411/135731998-e01a7970-a7c4-4041-b07c-341e075207d2.png",
-                "https://user-images.githubusercontent.com/39378411/135731990-8f64ce8e-d655-4ded-9561-3d7f6893d06e.png",
-              ]}
               overlapIcons={true}
             />
           </>
@@ -143,96 +160,47 @@ function App() {
 
       <GlSectionDivider />
 
-      <GlArticle
-        title="Article title"
-        body={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
-                    nisl nec **hendrerit** rutrum, 
-                    mi enim semper arcu, ut imperdiet urna libero non metus. 
-                    Donec imperdiet ac nulla sit amet lacinia. 
-                    Suspendisse volutpat lectus vitae libero luctus, a egestas magna egestas. 
-                    Suspendisse potenti. In semper erat scelerisque sapien convallis porttitor.`}
-        buttonLabel="Article Button label"
-        buttonLink={{
-          href: "https://example.com",
-        }}
-        illustration={
-          <GlIllustration
-            type="image"
-            url="https://user-images.githubusercontent.com/39378411/135731816-5ba39459-d95e-413d-b515-92a7b0dc5acf.png"
-            hasShadow={false}
-          />
-        }
-        hasAnimation={true}
-      />
-
-      <GlCards>
-        <>
-          <GlProjectCard
-            title="Project Card Title"
-            subtitle="Project Card Subtitle"
-            projectImageUrl="https://user-images.githubusercontent.com/39378411/135731821-0f1c7165-fada-418f-896a-c36a0e55171b.png"
-          />
-          <GlProjectCard
-            title="Project Card Title"
-            subtitle="Project Card Subtitle"
-            projectImageUrl="https://user-images.githubusercontent.com/39378411/135731810-93193eba-ada5-460a-96ad-948215278850.png"
-          />
-          <GlProjectCard
-            title="Project Card Title"
-            subtitle="Project Card Subtitle"
-            projectImageUrl="https://user-images.githubusercontent.com/39378411/135731819-1cf18697-e946-449f-ad6e-be414e341c40.png"
-          />
-          <GlProjectCard
-            title="Project Card Title"
-            subtitle="Project Card Subtitle"
-            projectImageUrl="https://user-images.githubusercontent.com/39378411/135731914-0265b77e-3338-4747-b014-e845f16332a7.png"
-          />
-        </>
-      </GlCards>
-
-      <GlSectionDivider />
-
       <GlCheckList
-        heading="Check List Heading"
+        heading="Features"
         hasAnimation={true}
         elements={[
           {
-            title: "List element title",
+            title: "LoRaWAN",
             description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
                                 nisl nec hendrerit rutrum, 
                                 mi enim semper arcu, ut **imperdiet** urna libero non metus. 
                                 Donec imperdiet ac nulla sit amet lacinia.`,
           },
           {
-            title: "List element title",
+            title: "Low Power",
             description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
                                 nisl nec hendrerit rutrum, 
                                 mi enim semper arcu, ut imperdiet urna libero non metus. 
                                 Donec imperdiet ac nulla sit amet lacinia.`,
           },
           {
-            title: "List element title",
+            title: "Maintenance-Free",
             description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
                                 nisl nec hendrerit rutrum, 
                                 mi enim semper arcu, ut imperdiet urna libero non metus. 
                                 Donec imperdiet ac nulla sit amet lacinia.`,
           },
           {
-            title: "List element title",
+            title: "Shell Interface",
             description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
                                 nisl nec hendrerit rutrum, 
                                 mi enim semper arcu, ut imperdiet urna libero non metus. 
                                 Donec imperdiet ac nulla sit amet lacinia.`,
           },
           {
-            title: "List element title",
+            title: "Real-Time & Historical Data",
             description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
                                 nisl nec hendrerit rutrum, 
                                 mi enim semper arcu, ut imperdiet urna libero non metus. 
                                 Donec imperdiet ac nulla sit amet lacinia.`,
           },
           {
-            title: "List element title",
+            title: "Map Overlay",
             description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
                                 nisl nec hendrerit rutrum, 
                                 mi enim semper arcu, ut imperdiet urna libero non metus. 
@@ -240,6 +208,131 @@ function App() {
           },
         ]}
       />
+
+      <GlSectionDivider />
+
+      <GlCards
+        title="The Team"
+      >
+        <>
+          <GlProjectCard
+            title="Paul Wood"
+            subtitle="Project Card Subtitle"
+            projectImageUrl="https://user-images.githubusercontent.com/39378411/135731821-0f1c7165-fada-418f-896a-c36a0e55171b.png"
+          />
+          <GlProjectCard
+            title="Ian Wallace"
+            subtitle="Project Card Subtitle"
+            projectImageUrl="https://user-images.githubusercontent.com/39378411/135731810-93193eba-ada5-460a-96ad-948215278850.png"
+          />
+          <GlProjectCard
+            title="Randy Alvarez"
+            subtitle="Project Card Subtitle"
+            projectImageUrl="https://user-images.githubusercontent.com/39378411/135731819-1cf18697-e946-449f-ad6e-be414e341c40.png"
+          />
+          <GlProjectCard
+            title="Parke Benjamin"
+            subtitle="Project Card Subtitle"
+            projectImageUrl="https://user-images.githubusercontent.com/39378411/135731914-0265b77e-3338-4747-b014-e845f16332a7.png"
+          />
+        </>
+      </GlCards>
+
+      <GlCards
+        title="Project Documentation"
+      >
+        {
+          <>
+            <GlLogoCard
+              title="Conference Paper"
+              paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
+                                nisl nec hendrerit rutrum, 
+                                mi enim semper arcu, ut imperdiet urna libero non metus.`}
+              buttonLabel="Download"
+            />
+            <GlLogoCard
+              title="Report"
+              paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
+                                nisl nec hendrerit rutrum, 
+                                mi enim semper arcu, ut imperdiet urna libero non metus.`}
+              buttonLabel="Download"
+            />
+
+            <GlLogoCard
+              title="Presentation"
+              paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
+                                nisl nec hendrerit rutrum, 
+                                mi enim semper arcu, ut imperdiet urna libero non metus.`}
+              buttonLabel="Download"
+              overlapIcons={true}
+            />
+          </>
+        }
+      </GlCards>
+
+      <GlCards
+        title="Senior Design 1 Project Documentation"
+      >
+        {
+          <>
+            <GlLogoCard
+              title="CDR"
+              paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
+                                nisl nec hendrerit rutrum, 
+                                mi enim semper arcu, ut imperdiet urna libero non metus.`}
+              buttonLabel="Download"
+            />
+            <GlLogoCard
+              title="Report"
+              paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
+                                nisl nec hendrerit rutrum, 
+                                mi enim semper arcu, ut imperdiet urna libero non metus.`}
+              buttonLabel="Download"
+            />
+
+            <GlLogoCard
+              title="Project Report (SD1 Version)"
+              paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
+                                nisl nec hendrerit rutrum, 
+                                mi enim semper arcu, ut imperdiet urna libero non metus.`}
+              buttonLabel="Download"
+              overlapIcons={true}
+            />
+          </>
+        }
+      </GlCards>
+
+      <GlCards
+        title="Project Source"
+      >
+        {
+          <>
+            <GlLogoCard
+              title="Firmware"
+              paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
+                                nisl nec hendrerit rutrum, 
+                                mi enim semper arcu, ut imperdiet urna libero non metus.`}
+              buttonLabel="Download"
+            />
+            <GlLogoCard
+              title="Web Application"
+              paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
+                                nisl nec hendrerit rutrum, 
+                                mi enim semper arcu, ut imperdiet urna libero non metus.`}
+              buttonLabel="Download"
+            />
+
+            <GlLogoCard
+              title="Schematic & PCB"
+              paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
+                                nisl nec hendrerit rutrum, 
+                                mi enim semper arcu, ut imperdiet urna libero non metus.`}
+              buttonLabel="Download"
+              overlapIcons={true}
+            />
+          </>
+        }
+      </GlCards>
 
     </GlTemplate>
   );
