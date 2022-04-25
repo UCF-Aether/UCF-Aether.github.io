33b65d637d8aa397c94b321e4f6f9780fedda154
Paul Wood <pmwood@pm.me>
2022-04-25 02:11:23 -0400

    Filled in descriptions

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { GlArticle } from "gitlanding/GlArticle";
 import { GlCards } from "gitlanding/GlCards";
 import { GlLogoCard } from "gitlanding/GlCards/GlLogoCard";
-import { GlProjectCard } from "gitlanding/GlCards/GlProjectCard";
 import { GlCheckList } from "gitlanding/GlCheckList";
 import { GlFooter } from "gitlanding/GlFooter";
 import { GlHeader } from "gitlanding/GlHeader";
@@ -19,7 +18,7 @@ function App() {
     <GlTemplate
       header={
         <GlHeader
-          title="Aether Sensor Network"
+          title="Aether - Air Quality Sensor"
           links={[
             {
               label: "Report",
@@ -70,17 +69,15 @@ function App() {
       }
     >
       <GlHero
-        title="Aether Sensor Network"
-        subTitle="The Spring 2022 UCF CECS Senior Design Showcase winner, a LoRaWAN-enabled air quality sensor that measures and calculates the EPA Air Quality Index (AQI)."
+        title="A LoRaWAN-enabled air quality sensor"
+        subTitle="The Spring 2022 UCF CECS Senior Design Showcase winner"
         illustration={{
           type: "image",
           imageSrc: "img/aether_node_1.png",
         }}
         hasLinkToSectionBellow={true}
-        hasIllustrationShadow={false}
+        hasIllustrationShadow={true}
       />
-
-
 
       {/* <GlArticle
         id="aboutAqi"
@@ -112,19 +109,21 @@ function App() {
       <GlArticle
         id="aboutAether"
         title="What is Aether?"
-        body={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
-                    nisl nec hendrerit rutrum, 
-                    mi enim **semper arcu**, ut imperdiet urna libero non metus. 
-                    Donec imperdiet ac nulla sit amet lacinia. 
-                    Suspendisse volutpat lectus vitae libero luctus, a egestas magna egestas. 
-                    Suspendisse potenti. In semper erat scelerisque sapien convallis porttitor.`}
+        body={`Aether is a low-power air quality monitoring device and web application that measures 
+          and calculates the EPA Air Quality Index (AQI). The Aether Node is implemented on a 
+          four-layer PCB and housed in a 3D printed enclosure. And, the
+          computation that is performed by the Aether Node is done by a
+          Seeed LoRa-E5 LoRaWAN module. This module packages an
+          ST STM32WLE5JC, which is an SoC combing an ARM M4
+          processor and a LoRa transceiver on the same IC. Packets are sent to The Things Network where
+          they are forwarded to Supabase, our backend, which allows data to be displayed on the web application hosted on AWS.`}
         buttonLabel="Aether GitHub"
         buttonLink={{
           href: "https://www.github.com/ucf-aether",
         }}
         illustration={
           <GlIllustration
-            hasShadow={false}
+            hasShadow={true}
             type="image"
             url="img/aetherOpenedUp.jpg"
           />
@@ -144,9 +143,10 @@ function App() {
           <>
             <GlLogoCard
               title="Designed in KiCad"
-              iconUrls={[
-                "img/kicad.png",  
-              ]}
+              link={{
+                href: "https://www.kicad.org",
+              }}
+              iconUrls={["img/kicad.png"]}
               paragraph={`The Aether schematic PCB was designed in KiCad 6. KiCad provides an extensive 
               part library which minimized the development of part footprints. Using KiCad allowed us 
               to version control our hardware design which allowed us to revert mistakes and easily 
@@ -154,9 +154,10 @@ function App() {
             />
             <GlLogoCard
               title="Powered by Zephyr RTOS"
-              iconUrls={[
-                "img/zephyr.png",  
-              ]}
+              iconUrls={["img/zephyr.png"]}
+              link={{
+                href: "https://www.zephyrproject.org",
+              }}
               paragraph={`Zephyr RTOS is an open, collaborative, vendor-neutral RTOS hosted under the
               Linux Foundation. Zephyr uses the Linux device tree and Kconfig specification for Generalizing
               drivers to work across architectures and boards. We were able to create a Aether specific 
@@ -167,13 +168,15 @@ function App() {
 
             <GlLogoCard
               title="Accessible through AWS & Supabase"
-              iconUrls={[
-                "img/aws.svg",  
-                "img/supabase.svg",  
-              ]}
-              paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
-                                nisl nec hendrerit rutrum, 
-                                mi enim semper arcu, ut imperdiet urna libero non metus.`}
+              iconUrls={["img/aws.svg", "img/supabase.svg"]}
+              link={{
+                href: "https://supabase.com",
+              }}
+              paragraph={`The web application is hosted on AWS, and the backend and API is hosted on Supabase,
+              a managed PostgreSQL database with PostgREST and HTTP extensions. The database is accessible
+              through Supabase's JavaScript client which allows for CRUD operations along with real-time
+              subscriptions to table inserts, updates, and deletions. All of the backend logic is 
+              performed inside Supabase and the PostgreSQL database.`}
               overlapIcons={false}
             />
           </>
@@ -188,54 +191,40 @@ function App() {
         elements={[
           {
             title: "LoRaWAN",
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
-                                nisl nec hendrerit rutrum, 
-                                mi enim semper arcu, ut **imperdiet** urna libero non metus. 
-                                Donec imperdiet ac nulla sit amet lacinia.`,
+            description: `The LoRa-E5 utilizes the STM32WLE5JC which combines a Cortex-M4 with a 
+              LoRa transceiver capable of LoRaWAN 1.4.`,
           },
           {
             title: "Low Power",
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
-                                nisl nec hendrerit rutrum, 
-                                mi enim semper arcu, ut imperdiet urna libero non metus. 
-                                Donec imperdiet ac nulla sit amet lacinia.`,
+            description: `Even with power hungry sensors, the device still operates at less than 100 mW
+              thanks to our power management.`,
           },
           {
             title: "Maintenance-Free",
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
-                                nisl nec hendrerit rutrum, 
-                                mi enim semper arcu, ut imperdiet urna libero non metus. 
-                                Donec imperdiet ac nulla sit amet lacinia.`,
+            description: `The Aether Node fits inside a small enclosure which makes it easy to mount
+              anywhere. The solar panel ensures longevity with little to no maintenance.`,
           },
           {
             title: "Shell Interface",
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
-                                nisl nec hendrerit rutrum, 
-                                mi enim semper arcu, ut imperdiet urna libero non metus. 
-                                Donec imperdiet ac nulla sit amet lacinia.`,
+            description: `Thanks to Zephyr, the Aether Node has a familiar shell interface that allows 
+              for user configuration and real-time device information.`,
           },
           {
             title: "Real-Time & Historical Data",
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
-                                nisl nec hendrerit rutrum, 
-                                mi enim semper arcu, ut imperdiet urna libero non metus. 
-                                Donec imperdiet ac nulla sit amet lacinia.`,
+            description: `The Aether web interface allows users to view historical and real-time AQI, 
+            temperature, humidity, ozone, and particulate matter.`,
           },
           {
             title: "Map Overlay",
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus, 
-                                nisl nec hendrerit rutrum, 
-                                mi enim semper arcu, ut imperdiet urna libero non metus. 
-                                Donec imperdiet ac nulla sit amet lacinia.`,
+            description: `The map overlay makes it easy to see what the current AQI value is in your 
+              area with a slider to view historical data.`,
           },
         ]}
       />
 
       <GlSectionDivider />
 
-      <GlCards
-        title="The Team"
-      >
+      <GlCards title="The Team">
         <>
           <PersonCard
             title="Paul Wood"
@@ -286,144 +275,139 @@ function App() {
         hasAnimation={true}
       />
 
-      <GlCards
-        title="Project Documentation"
-      >
+      <GlCards title="Project Documentation">
         {
           <>
             <MarkdownCard
               title="Conference Paper"
               buttonLabel="Download"
-              link={{ 
-                href: "docs/Conference_Paper_g41.pdf"
+              link={{
+                href: "docs/Conference_Paper_g41.pdf",
               }}
             />
             <MarkdownCard
               title="Final Report"
               buttonLabel="Download"
-              link={{ 
-                href: "docs/Final_Report_g41.pdf"
+              link={{
+                href: "docs/Final_Report_g41.pdf",
               }}
             />
 
             <MarkdownCard
               title="Final Presentation"
               buttonLabel="Download"
-              link={{ 
-                href: "docs/Final_Presentation_g41.pptx"
+              link={{
+                href: "docs/Final_Presentation_g41.pptx",
               }}
             />
             <MarkdownCard
               title="Schematic"
               buttonLabel="Download"
-              link={{ 
-                href: "docs/Schematic_g41.pdf"
+              link={{
+                href: "docs/Schematic_g41.pdf",
               }}
             />
             <MarkdownCard
               title="CDR Presentation"
               buttonLabel="Download"
-              link={{ 
-                href: "docs/CDR_Presentation_g41.pdf"
+              link={{
+                href: "docs/CDR_Presentation_g41.pdf",
               }}
             />
             <MarkdownCard
               title="SD1 Final Report"
               buttonLabel="Download"
-              link={{ 
-                href: "docs/Senior_Design_1_Final_Report_g41.pdf"
+              link={{
+                href: "docs/Senior_Design_1_Final_Report_g41.pdf",
               }}
             />
 
             <MarkdownCard
               title="Divide and Conquer (V2)"
               buttonLabel="Download"
-              link={{ 
-                href: "docs/Divide_and_Conquer_v2_g41.pdf"
+              link={{
+                href: "docs/Divide_and_Conquer_v2_g41.pdf",
               }}
             />
             <MarkdownCard
               title="Divide and Conquer (V1)"
               buttonLabel="Download"
-              link={{ 
-                href: "docs/Divide_and_Conquer_v1_g41.pdf"
+              link={{
+                href: "docs/Divide_and_Conquer_v1_g41.pdf",
               }}
             />
           </>
         }
       </GlCards>
 
-      <GlCards
-        title="Project Source"
-      >
+      <GlCards title="Project Source">
         {
           <>
             <MarkdownCard
               title="Firmware"
               github="ucf-aether/aether-firmware"
               buttonLabel="Download"
-              link={{ 
-                href: "src/Firmware_g41.zip"
+              link={{
+                href: "src/Firmware_g41.zip",
               }}
             />
             <MarkdownCard
               title="Web Application"
               github="ucf-aether/aether-app"
               buttonLabel="Download"
-              link={{ 
-                href: "src/App_g41.zip"
+              link={{
+                href: "src/App_g41.zip",
               }}
             />
             <MarkdownCard
               title="Schematic & PCB"
               github="ucf-aether/electronics"
               buttonLabel="Download"
-              link={{ 
-                href: "src/Electronics_g41.zip"
+              link={{
+                href: "src/Electronics_g41.zip",
               }}
             />
             <MarkdownCard
               title="Final Report"
               github="ucf-aether/report"
               buttonLabel="Download"
-              link={{ 
-                href: "src/Final_Report_g41.zip"
+              link={{
+                href: "src/Final_Report_g41.zip",
               }}
             />
             <MarkdownCard
               title="Conference Paper"
               github="ucf-aether/conference-paper"
               buttonLabel="Download"
-              link={{ 
-                href: "src/Conference_Paper_g41.zip"
+              link={{
+                href: "src/Conference_Paper_g41.zip",
               }}
             />
             <MarkdownCard
               title="SD1 Final Report"
               github="ucf-aether/report/tree/vSD1"
               buttonLabel="Download"
-              link={{ 
-                href: "src/Senior_Design_1_Final_Report_g41.zip"
+              link={{
+                href: "src/Senior_Design_1_Final_Report_g41.zip",
               }}
             />
             <MarkdownCard
               title="Divide and Conquer (V2)"
               buttonLabel="Download"
-              link={{ 
-                href: "src/Divide_and_Conquer_v2_g41.zip"
+              link={{
+                href: "src/Divide_and_Conquer_v2_g41.zip",
               }}
             />
             <MarkdownCard
               title="Divide and Conquer (V1)"
               buttonLabel="Download"
-              link={{ 
-                href: "src/Divide_and_Conquer_v1_g41.zip"
+              link={{
+                href: "src/Divide_and_Conquer_v1_g41.zip",
               }}
             />
           </>
         }
       </GlCards>
-
     </GlTemplate>
   );
 }
